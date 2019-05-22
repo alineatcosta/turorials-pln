2072de0ef90a74c1dc4428d3ae642acaef030fb2
Aline Costa <aline.costa.cc@gmail.com>
2019-05-22 17:00:06 -0300

    Iniciando exemplo com aprendizagem não-supervisionada

--- exemplo-unsupervised-nl.py
+++ exemplo-unsupervised-nl.py
@@ -1,6 +1,32 @@
-# clean() is a modified version of extract_signature() found in bruteforce.py in the GitHub repository linked above
-cleaned_email, _ = clean(email)
+# LINK: https://medium.com/jatana/unsupervised-text-summarization-using-sentence-embeddings-adb15ce83db1
 
-lines = cleaned_email.split('\n')
-lines = [line for line in lines if line != '']
-cleaned_email = ' '.join(lines)
+# Step-1: Email Cleaning
+from talon.signature.bruteforce import extract_signature
+
+email = 'Hey man! How r u?\nOther text\n\n--\n\nAqui mais texto\nRegards,\nRoman'
+
+cleaned_email, _ = extract_signature(email)
+
+print(cleaned_email)
+
+# Step-2: Language Detection
+# it is not necessary
+
+# Step-3: Sentence Tokenization with NLTK
+from nltk.tokenize import sent_tokenize
+sentences = sent_tokenize(cleaned_email, language = 'english')
+
+print(sentences)
+
+# Step-4: Skip-Thought Encoder (codificador)
+# We'll use the model  Word2Vec Skip-Gram treinado para prever as palavras em torno de uma palavra de entrada.
+
+# The 'skipthoughts' module can be found at the root of the GitHub repository linked above
+import skipthoughts
+
+# You would need to download pre-trained models first
+# https://github.com/jatana-research/email-summarization
+model = skipthoughts.load_model()
+
+encoder = skipthoughts.Encoder(model)
+encoded =  encoder.encode(sentences)
